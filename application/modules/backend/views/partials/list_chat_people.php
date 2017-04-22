119144c6dbbb86d4b83609e982973e03f43f5979
Nushrat-Perween <nushahmad04@gmail.com>
2017-04-22 18:00:28 +0530

    update

--- application/modules/backend/views/partials/list_chat_people.php
+++ application/modules/backend/views/partials/list_chat_people.php
@@ -11,7 +11,7 @@
 <?php foreach($chat_admin as $row) {
 	if($row['is_logged_in'] == 1) {
 		?>
-                <a href="javascript:;" data-toggle="modal" data-target=".chat-message" onclick="open_chat_box(`<?php echo $row['id'];?>`,`<?php echo $row['first_name']." ".$row['last_name'];?>`);">
+                <a href="javascript:;" data-toggle="modal" data-target=".chat-message" onclick="open_chat_box(`<?php echo $row['id'];?>`,`<?php echo $row['first_name']." ".$row['last_name'];?>`,`<?php if($row['last_visit_date'] != "0000-00-00 00:00:00") { echo date('d-m-Y g:i A',strtotime($row['last_visit_date']));} else { echo "NA";}?>`);">
                   <span class="status-online"></span>
                   <span><?php echo $row['first_name']." ".$row['last_name'];?></span>
                 </a>
@@ -26,7 +26,7 @@
                   <?php foreach($chat_admin as $row) {
                	 	if($row['is_logged_in'] == 0) {
                 ?>
-                <a href="javascript:;" data-toggle="modal" data-target=".chat-message" onclick="open_chat_box(`<?php echo $row['id'];?>`,`<?php echo $row['first_name']." ".$row['last_name'];?>`);">
+                <a href="javascript:;" data-toggle="modal" data-target=".chat-message" onclick="open_chat_box(`<?php echo $row['id'];?>`,`<?php echo $row['first_name']." ".$row['last_name'];?>`,`<?php if($row['last_visit_date'] != "0000-00-00 00:00:00") { echo date('d-m-Y g:i A',strtotime($row['last_visit_date']));} else { echo "NA";}?>`);">
                   <span class="status-offline"></span>
                   <span><?php echo $row['first_name']." ".$row['last_name'];?></span>
                 </a>
